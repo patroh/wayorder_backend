c052a2daa747e4d484215b76dc78076dce2bed65
Rohan Patel <patel969@sheridancollege.ca>
2020-05-14 20:13:57 -0400

    added desc

--- src/main/java/com/capstone/project/Bean/Dish.java
+++ src/main/java/com/capstone/project/Bean/Dish.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Data
@@ -22,6 +19,8 @@ public class Dish {
     private Long id;
 
     private String name;
+    @Lob
+    private String description;
     private Float price;
     private Float star;
     private Integer reviews;
